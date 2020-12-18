16d84151c1faece359fbdebf02cbea89bb0d229a
FrenchKrab <krabygames@gmail.com>
2020-12-19 00:13:14 +0100

    maven project now creates valid plugin jar

---
+++ mcarcinizer/src/main/java/org/kraby/mcarcinizer/CarcinizerMain.java
@@ -0,0 +1,31 @@
+package org.kraby.mcarcinizer;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+public class CarcinizerMain extends JavaPlugin
+{
+	private static CarcinizerMain singleton;
+	
+	@Override
+	public void onEnable() {
+		super.onEnable();
+		setSingleton(this);
+	}
+	
+	
+	@Override
+	public void onDisable()
+	{
+		//
+	}
+	
+
+	private static void setSingleton(CarcinizerMain singleton) {
+		CarcinizerMain.singleton = singleton;
+	}
+
+	public static CarcinizerMain getSingleton() {
+		return singleton;
+	}
+}
