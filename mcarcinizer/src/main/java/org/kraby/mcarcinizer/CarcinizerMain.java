f099b43e0c4e5ac1eafa31ac52c687f9f2fc056c
FrenchKrab <krabygames@gmail.com>
2020-12-19 01:02:20 +0100

    add checkstyle + CI

--- mcarcinizer/src/main/java/org/kraby/mcarcinizer/CarcinizerMain.java
+++ mcarcinizer/src/main/java/org/kraby/mcarcinizer/CarcinizerMain.java
@@ -3,29 +3,27 @@ package org.kraby.mcarcinizer;
 import org.bukkit.plugin.java.JavaPlugin;
 
 
-public class CarcinizerMain extends JavaPlugin
-{
-	private static CarcinizerMain singleton;
-	
-	@Override
-	public void onEnable() {
-		super.onEnable();
-		setSingleton(this);
-	}
-	
-	
-	@Override
-	public void onDisable()
-	{
-		//
-	}
-	
+public class CarcinizerMain extends JavaPlugin {
+    private static CarcinizerMain singleton;
+    
+    @Override
+    public void onEnable() {
+        super.onEnable();
+        setSingleton(this);
+    }
+    
+    
+    @Override
+    public void onDisable() {
+        //
+    }
+    
 
-	private static void setSingleton(CarcinizerMain singleton) {
-		CarcinizerMain.singleton = singleton;
-	}
+    private static void setSingleton(CarcinizerMain singleton) {
+        CarcinizerMain.singleton = singleton;
+    }
 
-	public static CarcinizerMain getSingleton() {
-		return singleton;
-	}
+    public static CarcinizerMain getSingleton() {
+        return singleton;
+    }
 }
