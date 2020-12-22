8993081e65f07380711f8205195443098063d44b
FrenchKrab <krabygames@gmail.com>
2020-12-22 12:10:21 +0100

    plug deathkeeper subplugin into main plugin

--- mcarcinizer/src/main/java/org/kraby/mcarcinizer/CarcinizerMain.java
+++ mcarcinizer/src/main/java/org/kraby/mcarcinizer/CarcinizerMain.java
@@ -1,23 +1,35 @@
 package org.kraby.mcarcinizer;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.kraby.mcarcinizer.deathkeeper.DeathKeeperSubplugin;
 
 
 public class CarcinizerMain extends JavaPlugin {
     private static CarcinizerMain singleton;
-    
+
+    private List<Subplugin> subplugins = new ArrayList<>();
+
     @Override
     public void onEnable() {
         super.onEnable();
         setSingleton(this);
+        initializeSubPlugins();
     }
-    
-    
+
+
     @Override
     public void onDisable() {
         //
     }
-    
+
+
+
+    private void initializeSubPlugins() {
+        subplugins.add(new DeathKeeperSubplugin(this));
+    }
+
 
     private static void setSingleton(CarcinizerMain singleton) {
         CarcinizerMain.singleton = singleton;
