8c9a23356f05b1b6037f5fcff3e108d3c6202498
FrenchKrab <krabygames@gmail.com>
2021-04-11 13:10:10 +0200

    add eventual cfg warnings in carcreload

--- mcarcinizer/src/main/java/com/kraby/mcarcinizer/mcarcinizer/commands/ReloadCommand.java
+++ mcarcinizer/src/main/java/com/kraby/mcarcinizer/mcarcinizer/commands/ReloadCommand.java
@@ -1,6 +1,9 @@
 package com.kraby.mcarcinizer.mcarcinizer.commands;
 
+
+import java.util.List;
 import com.kraby.mcarcinizer.CarcinizerMain;
+import com.kraby.mcarcinizer.mcarcinizer.InfoLogs;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -12,6 +15,10 @@ public class ReloadCommand implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         CarcinizerMain.getSingleton().reloadSubplugin();
         sender.sendMessage(ChatColor.GREEN + "MCarcinizer configs reloaded");
+
+        List<String> cfgErrors = CarcinizerMain.getSingleton().getConfigErrors();
+        InfoLogs.warnIfConfigsContainErrors(sender, cfgErrors.size());
+
         return true;
     }
 
