62f573548f1c2b41894b661d78012c03b0f9d9bd
FrenchKrab <krabygames@gmail.com>
2021-04-11 13:07:12 +0200

    add getConfigErrors method in CarcinizerMain

--- mcarcinizer/src/main/java/com/kraby/mcarcinizer/CarcinizerMain.java
+++ mcarcinizer/src/main/java/com/kraby/mcarcinizer/CarcinizerMain.java
@@ -1,11 +1,17 @@
 package com.kraby.mcarcinizer;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import com.kraby.mcarcinizer.deathkeeper.DeathKeeperSubplugin;
 import com.kraby.mcarcinizer.healthhardcorizer.HealthHardcorizerSubplugin;
 import com.kraby.mcarcinizer.mcarcinizer.commands.InvSizeCommand;
+import com.kraby.mcarcinizer.mcarcinizer.InfoLogs;
+import com.kraby.mcarcinizer.mcarcinizer.commands.CheckConfigCommand;
 import com.kraby.mcarcinizer.mcarcinizer.commands.ReloadCommand;
+import com.kraby.mcarcinizer.utils.config.ConfigAccessor;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 
@@ -49,6 +55,24 @@ public class CarcinizerMain extends JavaPlugin {
     private void registerCommands() {
         this.getCommand("carcreload").setExecutor(new ReloadCommand());
         this.getCommand("invsize").setExecutor(new InvSizeCommand());
+    }
+
+
+    /**
+     * Returns all errors found in subplugins configurations.
+     * @return a non null list.
+     */
+    public List<String> getConfigErrors() {
+        List<String> errors = new ArrayList<>();
+        for (Subplugin sp : this.getSubplugins()) {
+            String pluginPrefix = ChatColor.YELLOW + "(" + sp.getName() + ") ";
+            for (ConfigAccessor conf : sp.getConfigAccessors()) {
+                for (String error : conf.getErrors()) {
+                    errors.add(pluginPrefix + ChatColor.RED + error);
+                }
+            }
+        }
+        return errors;
     }
 
 
